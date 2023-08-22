b9f5f9bbca7602cd0cf21ab87ca14e08db90ed5a
Marko Dankovich <markodankovic@protonmail.com>
2023-08-22 21:33:48 +0200

    Post method implementation

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,5 +34,45 @@ namespace API.Controllers
             return await Context.Products.FindAsync(id);
 
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            if(product == null)
+            {
+                return BadRequest("Invalid product data");
+            }
+
+            Context.Products.Add(product);
+            await Context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id}, product);
+        }
+
+        public async Task<IActionResult> UpdateProduct(int id, [FromBody] Product product)
+        {
+            if(product == null || id != product.Id)
+            {
+                return BadRequest("Invalid product data or mismatched IDs");
+            }
+
+            var existingProduct = await Context.Products.FindAsync(id);
+            
+            if(existingProduct == null){
+
+                return NotFound("Product not found.");
+
+            }
+
+            //Update properties of the existing product from the provided product
+            Context.Entry(existingProduct).CurrentValues.SetValues(product);
+
+            await Context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
     }
 }
